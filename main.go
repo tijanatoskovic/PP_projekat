bb1523841bed30836bf13209a9efbeca14a97d49
tijana <tijanatoskovic02@gmail.com>
2024-04-10 21:57:06 +0200

    Added encryption and decryption of Single Word Text

--- main.go
+++ main.go
@@ -15,17 +15,38 @@ We will again ask for the password, then we will read the nonce by
 reading the last 12 digits of the encrypted file
 */
 
+/*
+	We will be encrypting and decrypting a text passed by the user
+	USING THE CEASER CIPHER
+	For example: 	HELLOWORLD		our key will be for now: 5
+
+	the original text: ABCDEFGHIJKLMOPQRSTUVWXYZ
+				26 - (key)5 = 21	/		\
+								  /			  \
+					ABCDEFGHIJKLMOPQRSTU	  VWXYZ
+	the hashed text:	VWXYZABCDEFGHIJKLMOPQRSTU
+
+		index of H in original text: 8
+		and we aplly the formula:
+		pos + len(original letters)) % len(original letters)
+		In put case:	(8+26)%26 = 8
+		H -----> C
+*/
+
 package main
 
 import (
 	"bytes"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/tijanatoskovic/PP_projekat/filecrypt"
 
 	"golang.org/x/term"
 )
+
+const originalLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
 func main() {
 	if len(os.Args) < 2 {
@@ -38,14 +59,72 @@ func main() {
 	switch function {
 	case "help":
 		printHelp()
-	case "encrypt":
+	case "encryptFile":
 		encryptHandle()
-	case "decrypt":
+	case "decryptFile":
 		decryptHandle()
+	case "encryptText":
+		encryptedText := encryptText(5)
+		decryptText(5, encryptedText)
 	default:
-		fmt.Println("Run encrypt to encrypt a file, and decrypt to decrypt a file.")
+		fmt.Println("Run:\t\"go run . help\"\tfor usage.")
 		os.Exit(1)
 	}
+}
+
+func hashLetterFunc(key int, letter string) (result string) {
+	//this func returns the hashedText
+	runes := []rune(letter)
+	lastLetterKey := string(runes[len(letter)-key : len(letter)])
+	leftOverLetter := string(runes[0 : len(letter)-key])
+	return fmt.Sprintf(`%s%s`, lastLetterKey, leftOverLetter)
+	//NOTICE: We have to use `` charachers because we are using runes
+}
+
+func decryptText(key int, encryptedText string) (result string) {
+	hashLetter := hashLetterFunc(key, originalLetter)
+	var hashedString = ""
+	findOne := func(r rune) rune {
+		pos := strings.Index(hashLetter, string([]rune{r}))
+		if pos != -1 {
+			letterPos := (pos + len(originalLetter)) % len(originalLetter)
+			hashedString = hashedString + string(originalLetter[letterPos])
+		}
+		return r
+	}
+
+	strings.Map(findOne, encryptedText)
+	fmt.Println("Decrypted text: ", hashedString)
+	return hashedString
+}
+
+func encryptText(key int) (result string) {
+	fmt.Println("Input text: ")
+	var plainText string
+	_, err := fmt.Scan(&plainText)
+	//TODO: Make it accept a string containing space and other characters
+	if err != nil {
+		panic("Error!\n")
+	}
+	hashLetter := hashLetterFunc(key, originalLetter)
+	var hashedString = ""
+	//rune represents a single UNI-CODE character
+	// for example the letter A=65
+	findOne := func(r rune) rune {
+		pos := strings.Index(originalLetter, string([]rune{r}))
+		//We want to check if this letter exist in our originalText
+		if pos != -1 {
+			letterPos := (pos + len(originalLetter)) % len(originalLetter)
+			hashedString = hashedString + string(hashLetter[letterPos])
+		}
+		return r
+	}
+
+	strings.Map(findOne, plainText)
+	//this func takes one by one letters of our plainText and sends
+	//it to the func findOne
+	fmt.Println("Encrypted text: ", hashedString)
+	return hashedString
 }
 
 func printHelp() {
@@ -54,12 +133,13 @@ func printHelp() {
 	fmt.Println("")
 	fmt.Println("Usage:")
 	fmt.Println("")
-	fmt.Println("\tgo run . encrypt /path/to/your/file")
+	fmt.Println("\tgo run . encryptFile /path/to/your/file")
 	fmt.Println("")
 	fmt.Println("Commands:")
 	fmt.Println("")
-	fmt.Println("\t encrypt\tEncrypts a file given a password")
-	fmt.Println("\t decrypt\tTries to decrypt a file using a password")
+	fmt.Println("\t encryptFile\tEncrypts a file given a password")
+	fmt.Println("\t decryptFile\tTries to decrypt a file using a password")
+	fmt.Println("\t encryptText\tEncrypts a given text with ceaser cipher")
 	fmt.Println("\t help\t\tDisplays help text")
 	fmt.Println("")
 
