988af8ee4ad907a2116cea9147b61cee2d1a34a1
tijana <tijanatoskovic02@gmail.com>
2024-05-18 18:52:58 +0200

    File handler test

---
+++ files/fileHandler.go
@@ -0,0 +1,57 @@
+package files
+
+import (
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"os"
+)
+
+func savePrivateKeyToFile(privateKey *rsa.PrivateKey, filename string) error {
+	privateKeyBytes := x509.MarshalPKCS1PrivateKey(privateKey)
+	privateKeyPEM := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: privateKeyBytes,
+	}
+
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	if err := pem.Encode(file, privateKeyPEM); err != nil {
+		return err
+	}
+
+	fmt.Println("Private key saved to", filename)
+	return nil
+}
+
+func loadPrivateKeyFromFile(filename string) (*rsa.PrivateKey, error) {
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	block, _ := pem.Decode(file)
+	if block == nil {
+		return nil, errors.New("failed to decode PEM block containing private key")
+	}
+
+	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return privateKey, nil
+}
+
+func validateFile(file string) bool {
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
